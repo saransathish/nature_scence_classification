6a0f01192e39fbbb728ea4a935d03851a645986b
saran <saransathi1010@gmail.com>
2023-09-05 22:19:01 +0530

    changing

--- naturePrediction/settings.py
+++ naturePrediction/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-su*qmk@@&dbza1b=%@@!+9$l-3i&3o@g1-p8+p^(bl%^m7=!sb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -117,9 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles','static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
